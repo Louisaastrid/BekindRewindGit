015aae05206b3c8c194b92eddaa43809ac3c3784
Louisaastrid <60742071+Louisaastrid@users.noreply.github.com>
2020-03-19 10:41:40 +0100

    Initialisation
    Fichier  de la solution  générée avec  dépendances

---
+++ BeKindRewind/CatalogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeKindRewind
+{
+    class CatalogRepository :IDisposable
+    {
+
+
+        public CatalogRepository()
+        {
+
+        }
+    }
+}
---
+++ BeKindRewind/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+
+namespace BeKindRewind
+{
+    public class Category
+    {
+        public int category_id { get; set; }
+        public string name { get; set; }
+        public DateTime last_update { get; set; }
+        public Category() //requis par dapper 
+        {
+        
+        }
+
+
+    }
+}
